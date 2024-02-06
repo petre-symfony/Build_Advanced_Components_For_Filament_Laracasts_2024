20c28fd4fd6f2bb2173dbc7cd8f1a4f333b16fee
petrero <petrero@gmail.com>
2024-02-06 10:16:43 +0200

    2.12 Now I'm going to go into the test form component and we can pass in the input to the render function very similar to how we did it in the root. Let's pass it into the view. Now we can actually get rid of this function altogether and replace it with the test form.  In the view we now need to render this input. And now this looks exactly the same as how it was but it's actually wrapped in a LiveWire component.

--- app/Livewire/TestForm.php
+++ app/Livewire/TestForm.php
@@ -2,10 +2,17 @@
 
 namespace App\Livewire;
 
+use App\Components\TextInput;
 use Livewire\Component;
 
 class TestForm extends Component {
 	public function render() {
-		return view('livewire.test-form');
+		$input = TextInput::make('email')
+			->label('Email Address');
+
+
+		return view('livewire.test-form', [
+			'input' => $input
+		]);
 	}
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Components\TextInput;
+use App\Livewire\TestForm;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,11 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/demo', function () {
-	$input = TextInput::make('email')
-		->label('Email Address');
-
-	return view('demo', [
-		'input' => $input
-	]);
-});
+Route::get('/demo', TestForm::class);
