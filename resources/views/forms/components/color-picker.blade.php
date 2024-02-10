a92d8b606ff5c4b8ade9343f72f773cfabdbcae0
petrero <petrero@gmail.com>
2024-02-10 13:11:51 +0200

    6.10 Now this is working correctly, we can start integrating our external JavaScript library. So as a reminder, this is iroh.js, it's going to give us a little color wheel to use and also a shade slider here. Let's get started. Okay, so there are two ways to install this, one's from NPM and there's a CDN here.
     Now usually I would recommend NPM all the way. What I'm actually going to do today though is use the CDN and then in a later episode refactor to using NPM and extracting all of this into a local JavaScript file that can be served from our own web server instead of the CDN.
     And that's actually going to be using Filament's asset system. So let's just copy this script for now. This should do us nicely.
     So again, this isn't ideal, putting a CDN link inside the blade file. But this is only going to be temporary until we refactor this, so it should be okay.
     And let's see, they're getting started. So we want a div and we're going to be initializing this color picker on the div and passing in a selector here. So in my experience, it's best not to use IDs like this to initialize a form field.
     I like to actually use Alpine refs, so that's what we're going to do today. So we just need a simple div, we don't need this input anymore.
     So div and I'm going to give it an Alpine ref and a ref is basically a reference to this element that we can then access from the Alpine component. So I'm going to call this picker. And this should allow us to reference this element.

--- resources/views/forms/components/color-picker.blade.php
+++ resources/views/forms/components/color-picker.blade.php
@@ -1,11 +1,10 @@
+<script src="https://cdn.jsdelivr.net/npm/@jaames/iro@5"></script>
+
 <x-dynamic-component
     :component="$getFieldWrapperView()"
     :field="$field"
 >
   <div x-data="{ state: $wire.$entangle('{{ $getStatePath() }}') }">
-    <input
-        type="color"
-        x-model="state"
-    >
+    <div x-ref="picker"></div>
   </div>
 </x-dynamic-component>
