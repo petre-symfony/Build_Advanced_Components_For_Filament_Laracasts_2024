c39767099811c1ceda6fecfdead3488abac29bc7
petrero <petrero@gmail.com>
2024-02-10 16:37:53 +0200

    8.7 And maybe we want to even go further and when you hover over a color we want to show the raw color in a tooltip. Now filament does ship with a tooltip library and you can use it with the XTooltip directive in Alpine. So we can use XTooltip raw and then just pass in our state.

--- resources/views/infolists/components/color-entry.blade.php
+++ resources/views/infolists/components/color-entry.blade.php
@@ -1,5 +1,6 @@
 <x-dynamic-component :component="$getEntryWrapperView()" :entry="$entry">
   <div
+      x-tooltip.raw="{{ $getState() }}"
       class="h-5 w-5 rounded"
       style="background-color: {{ $getState() }}"
   >
