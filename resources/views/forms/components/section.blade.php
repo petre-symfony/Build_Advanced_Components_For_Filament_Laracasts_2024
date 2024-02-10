8cb1521f75d095582538292b0203dd5587b7f37c
petrero <petrero@gmail.com>
2024-02-10 14:27:17 +0200

    7.9 As you can see we do have our icon now. It is in the wrong place though so let's move this. Let's cut it. And this is our header section so it's got space Y2 so let's make a new level of nesting here because this is probably going to be aside the heading and the description rather than above it. Let's make this space Y2.
     Let's now make this flex and this container flex 1 and this to have a gap probably 3 or something. Now that was a little bit small so let's make this bigger. Let's make this 8 and we can make this text grey 500. Now we have our icon that is dynamically rendered. If we swap out our icon to the name of another hero icon that's been registered like star it just updates and everything is properly dynamic.

--- resources/views/forms/components/section.blade.php
+++ resources/views/forms/components/section.blade.php
@@ -1,15 +1,17 @@
 <div {{ $attributes->class(['divide-y border shadow-sm rounded-2xl']) }}>
-  @if ($icon = $getIcon())
-    @svg($icon, 'w-6 h-6')
-  @endif
-  
-  <div class="p-6 space-y-2">
-    <h2 class="text-2xl font-bold">
-      {{ $getHeading() }}
-    </h2>
-    <p class="text-sm text-gray-700">
-      {{ $getDescription() }}
-    </p>
+  <div class="p-6 flex gap-3">
+    @if ($icon = $getIcon())
+      @svg($icon, 'w-8 h-8 text-gray-500')
+    @endif
+
+    <div class="flex-1 space-y-2">
+      <h2 class="text-2xl font-bold">
+        {{ $getHeading() }}
+      </h2>
+      <p class="text-sm text-gray-700">
+        {{ $getDescription() }}
+      </p>
+    </div>
   </div>
 
   <div class="p-6 space-y-6">
