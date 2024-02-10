c7437e7d7364ada4ea0852f90f5045facd9ad739
petrero <petrero@gmail.com>
2024-02-10 14:17:28 +0200

    7.7 We could add something like a divider in between the header section and the content section. So we can do that by passing in classes to the attributes bag. This is similar to how a blade component would work. So divide y and that will create a horizontal divider.
     Let's add some padding to each of these containers, so padding 6 potentially. And this one we probably want some spacing, so space y 6. And let's wrap each of these in elements. Let's use h2 for this one. And a paragraph for the description.
     For the h2 I'm going to use text-2xl and font bold. And for the paragraph I'm going to use text small and text grey 700.
     I think that will create enough difference. Looking back at this I think space 6 could be quite a lot here. So let's use space 2. As you can see we've got our heading and our description styling.
     You probably want some sort of border around the entire section like we did in our example. So let's use border, shadow small. And let's also make it rounded like other filament components, rounded 2xl. So I think 2xl is definitely a Tailwind class but it's not been compiled into our style sheet. So we need to make sure that we're running Vite. Now there is a slightly different process when you are distributing a plugin with other Tailwind CSS classes in it. And that is to make sure that your users also have those classes compiled -- npm run build. But for now this is just a component that lives in our app so we don't have to worry about that. Let's check and it looks like the rounding has been applied perfectly.

--- resources/views/forms/components/section.blade.php
+++ resources/views/forms/components/section.blade.php
@@ -1,10 +1,14 @@
-<div {{ $attributes }}>
-  <div>
-    {{ $getHeading() }}
-    {{ $getDescription() }}
+<div {{ $attributes->class(['divide-y border shadow-sm rounded-2xl']) }}>
+  <div class="p-6 space-y-2">
+    <h2 class="text-2xl font-bold">
+      {{ $getHeading() }}
+    </h2>
+    <p class="text-sm text-gray-700">
+      {{ $getDescription() }}
+    </p>
   </div>
 
-  <div>
+  <div class="p-6 space-y-6">
     {{ $getChildComponentContainer() }}
   </div>
 </div>
