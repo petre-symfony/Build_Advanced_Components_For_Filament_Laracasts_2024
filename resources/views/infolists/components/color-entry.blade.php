328b4c78a70ba3600e9acc5a2432ec85e81b9665
petrero <petrero@gmail.com>
2024-02-10 16:43:32 +0200

    8.8 We also need to initialize an Alpine.js component so that we can use the tooltip.  So now let's hover over this and we have our raw value of the color. Now how do we handle the case where either one color can be passed in or indeed an array of colors. Let's leave this array here and think about that. So when we're passing in an array the state here in this variable is going to be an array too. So this is going to be our raw color codes.

--- resources/views/infolists/components/color-entry.blade.php
+++ resources/views/infolists/components/color-entry.blade.php
@@ -1,5 +1,8 @@
 <x-dynamic-component :component="$getEntryWrapperView()" :entry="$entry">
+  @dd($getState())
+  
   <div
+      x-data="{}"
       x-tooltip.raw="{{ $getState() }}"
       class="h-5 w-5 rounded"
       style="background-color: {{ $getState() }}"
