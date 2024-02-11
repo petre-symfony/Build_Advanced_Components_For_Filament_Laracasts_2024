123f1b052d302dd480d032a8aaa77025496c2c1b
petrero <petrero@gmail.com>
2024-02-11 13:48:28 +0200

    10.6 Let's remove that again. And similar to our InfoList entry, let's make this a block of colour.  So div, class, h5, width 5, and the style is going to be background colour, and then the state. As you can see our colours output nicely. Let's add a little bit of padding. So padding x4, padding y3. Now that looks quite aligned. And let's make it rounded as well. So rounded. Cool. So as you can tell, this is very similar to an InfoList entry. The difference is that with an InfoList entry, it's only going to be rendered once within the InfoList. Whereas with a table column, the object is going to be shared between each row in the table.  However, when we render each row, we are passing a different record to that table column object. And that then allows the table column to extract the colour from that record for the row and output it.

--- resources/views/tables/columns/color-column.blade.php
+++ resources/views/tables/columns/color-column.blade.php
@@ -1,5 +1,8 @@
-<div>
-  {{ dump($getRecord()) }}
-  
-  {{ $getState() }}
+<div class="px-4 py-3">
+  <div
+      class="h-5 w-5 rounded"
+      style="background-color: {{ $getState() }}"
+  >
+  </div>
+
 </div>
