c336e9de5165fb3f50cbd79dedffb4a3a454cfb5
petrero <petrero@gmail.com>
2024-02-27 11:43:18 +0200

    12.3 Now, as I mentioned earlier, we're going to be using these Sparty package tools. And this comes with a pre-built service provider.
     So let's make that service provider inside our source directory of the plugin. So new PHP class. I'm going to call it filament-toolkit-service-provider. The namespace is going to be dan-haran-filament-toolkit, like in our composer.json. And we're going to be extending Sparty's package service provider.
     Now, this has generated a class for us. And since this is an abstract class that has this configure package function, phpStorm has gone ahead and added this configure package method. If you're not using phpStorm, you might need to add this yourself. You can use the package service provider to look at the signature of this method and copy it over into your own service provider.

---
+++ packages/filament-toolkit/src/FilamentToolkitServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DanHarrin\FilamentToolkit;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class FilamentToolkitServiceProvider extends PackageServiceProvider {
+
+	public function configurePackage(Package $package): void {
+		// TODO: Implement configurePackage() method.
+	}
+}
