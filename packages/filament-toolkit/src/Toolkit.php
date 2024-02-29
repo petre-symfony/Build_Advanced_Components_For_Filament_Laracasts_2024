e8e18e3222c546bc58914bcebae8481508697f9a
petrero <petrero@gmail.com>
2024-02-29 11:54:09 +0200

    16.9 Once we have that, we can now chain on anything we want as configuration. So let's have a look at this resource. For example, maybe you want to toggle on and off this email verified at column and some users might want it in their panels and some others might not.  Let's add a new public function, email verified at, and this is going to accept a Boolean condition and going to return an instance of the plugin object. So we're going to return current object, store the condition, has email verified at.  Add a property here, so protected Boolean, has email verified at, set it to false. And then we need a getter. So public function has email verified at, it's going to return a Boolean of our property. So now what we can do is actually use this configuration method we've added to this plugin object right here. So we can go email verified at false, for example

--- packages/filament-toolkit/src/Toolkit.php
+++ packages/filament-toolkit/src/Toolkit.php
@@ -7,10 +7,22 @@ use Filament\Contracts\Plugin;
 use Filament\Panel;
 
 class Toolkit implements Plugin {
+	protected bool $hasEmailVerifiedAt = false;
+
 	public static function make(): Toolkit {
 		return new Toolkit();
 	}
 
+	public function emailVerifiedAt(bool $condition): static {
+		$this->hasEmailVerifiedAt = $condition;
+
+		return $this;
+	}
+
+	public function hasEmailVerifiedAt(): bool {
+		return $this->hasEmailVerifiedAt;
+	}
+	
 	public function getId(): string {
 		return 'danharrin-toolkit';
 	}
