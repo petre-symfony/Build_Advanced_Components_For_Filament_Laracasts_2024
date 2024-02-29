9b66f194864248725dfce4b24fa00e321769b77c
petrero <petrero@gmail.com>
2024-02-29 12:05:10 +0200

    16.11 And in our configuration, we can say email verified at true, and now it's showing.  Now since this plugin object, this instance here is specific to this panel, if the user has multiple panels in their apps, they can use this plugin across multiple panels, but with different configuration. So it gives you much easier flexibility than something like a traditional Laravel configuration file would, simply because it can be customized contextually based on the panel.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -33,7 +33,7 @@ class AdminPanelProvider extends PanelProvider
                 'primary' => Color::Amber,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
-						->plugin(Toolkit::make()->emailVerifiedAt(false))
+						->plugin(Toolkit::make()->emailVerifiedAt(true))
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
                 Pages\Dashboard::class,
